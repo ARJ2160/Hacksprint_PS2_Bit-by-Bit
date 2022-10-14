327a1eafcca34519323cbf7b5ae3803e5525f05c
Shreyas Kulkarni <shreyas290601@gmail.com>
2022-10-15 00:50:13 +0530

    exposed books API

--- server/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/hello")
-def hello_world():
-  return {"abcd" : "1234", "programming" : "python"}
- 
-if __name__ == '__main__':
-  app.run(port=8000,debug=True)
---
+++ server/back-end/app.py
@@ -0,0 +1,41 @@
+from flask import Flask,session,redirect,request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+# with open('/Hacksprint_PS2_Bit-by-Bit/server/imp.json') as important:
+#     imp = json.load(important)['params']
+app.config['SECRET_KEY'] = 'f16bbd80d59404'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Hacksprint"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# app.config.update(
+#     MAIL_SERVER='smtp.gmail.com',
+#     MAIL_PORT='465',
+#     MAIL_USE_SSL=True,
+#     MAIL_USERNAME=imp['account'],
+#     MAIL_PASSWORD=imp['password']
+# )
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+
+@app.route("/hello")
+def hello_world():
+  return {"abcd" : "1234", "programming" : "python"}
+ 
+
+@app.route('/abcd')
+def landing():
+  return{"Hello":"This works"}
+
+@app.route('/books',methods=['GET','POST'])
+def books():
+  store=[]
+  for post in db.onlinelibrary.find({}):
+    store.append(post)
+  return store[0:100]
+
+
+
+if __name__ == '__main__':
+  app.run(port=8000,debug=True)
