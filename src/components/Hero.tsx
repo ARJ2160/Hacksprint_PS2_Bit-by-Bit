96c3cef10d821c00b0c1b24988b9a6a97937b940
Atharva Joshi <atharvaj2160@gmail.com>
2022-10-14 12:53:37 +0530

    Text change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,15 +3,12 @@ import BookSVG from '../assets/books.svg';
 import BookData from "../assets/books.json"
 
 const Hero = () => {
-  const [books, setBooks] = useState()
-
   console.log(BookData);
-  console.log(books);
   return (
     <div className='App grid grid-cols-2 bg-bgColor h-screen p-5'>
       <div className='flex justify-center items-start flex-col pl-10 col-span-1 '>
-        <div className='font-medium lg:text-6xl md:text-4xl text-3xl mb-5 text-pinkRed'>
-          Online Library
+        <div className='font-medium lg:text-5xl md:text-4xl text-3xl mb-5 leading-5 tracking-wider  text-pinkRed uppercase'>
+         The Library thats always open. Shop Online with us
         </div>
         <div className='text-xl text-slate-700'>
           We believe a world-class library should be a direct reflection of the
