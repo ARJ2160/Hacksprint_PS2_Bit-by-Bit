435770812be822779bb01cc10b21562a6aa09564
Atharva Joshi <atharvaj2160@gmail.com>
2022-10-14 12:21:30 +0530

    Added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,18 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    axios.get("/register").then(res => console.log(res))
-  },[])
+  // useEffect(() => {
+  //   axios.get("/register").then(res => console.log(res))
+  // },[])
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
 }
 
-export default App
+export default App;
